373511b506104b90215328781fa04cf5e40b2ee1
JAEGEUN SONG <76800974+drow724@users.noreply.github.com>
2021-08-25 22:41:31 +0900

    API 인터페이스 및 결과 데이터 구조 설계

--- src/main/java/com/greeners/rest/api/controller/UserController.java
+++ src/main/java/com/greeners/rest/api/controller/UserController.java
@@ -1,15 +1,20 @@
 package com.greeners.rest.api.controller;
 
-import java.util.List;
-
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.greeners.rest.api.entity.User;
+import com.greeners.rest.api.model.response.CommonResult;
+import com.greeners.rest.api.model.response.ListResult;
+import com.greeners.rest.api.model.response.SingleResult;
 import com.greeners.rest.api.repository.UserRepository;
+import com.greeners.rest.api.service.ResponseService;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -22,22 +27,54 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping(value = "/v1")
 public class UserController {
 
-    private final UserRepository userRepository;
+    private final UserRepository userJpaRepo;
+    private final ResponseService responseService; // 결과를 처리할 Service
 
-    @ApiOperation(value = "회원 조회", notes = "모든 회원을 조회한다")
-    @GetMapping(value = "/user")
-    public List<User> findAllUser() {
-        return userRepository.findAll();
+    @ApiOperation(value = "회원 리스트 조회", notes = "모든 회원을 조회한다")
+    @GetMapping(value = "/users")
+    public ListResult<User> findAllUser() {
+        // 결과데이터가 여러건인경우 getListResult를 이용해서 결과를 출력한다.
+        return responseService.getListResult(userJpaRepo.findAll());
     }
 
-    @ApiOperation(value = "회원 입력", notes = "회원을 입력한다.")
+    @ApiOperation(value = "회원 단건 조회", notes = "userId로 회원을 조회한다")
+    @GetMapping(value = "/user/{msrl}")
+    public SingleResult<User> findUserById(@ApiParam(value = "회원ID", required = true) @PathVariable long msrl) {
+        // 결과데이터가 단일건인경우 getBasicResult를 이용해서 결과를 출력한다.
+        return responseService.getSingleResult(userJpaRepo.findById(msrl).orElse(null));
+    }
+
+    @ApiOperation(value = "회원 입력", notes = "회원을 입력한다")
     @PostMapping(value = "/user")
-    public User save(@ApiParam(value = "회원아이디", required = true) @RequestParam String uid,
-                     @ApiParam(value = "회원이름", required = true) @RequestParam String name) {
+    public SingleResult<User> save(@ApiParam(value = "회원아이디", required = true) @RequestParam String uid,
+                                   @ApiParam(value = "회원이름", required = true) @RequestParam String name) {
         User user = User.builder()
                 .uid(uid)
                 .name(name)
                 .build();
-        return userRepository.save(user);
+        return responseService.getSingleResult(userJpaRepo.save(user));
+    }
+
+    @ApiOperation(value = "회원 수정", notes = "회원정보를 수정한다")
+    @PutMapping(value = "/user")
+    public SingleResult<User> modify(
+            @ApiParam(value = "회원번호", required = true) @RequestParam long msrl,
+            @ApiParam(value = "회원아이디", required = true) @RequestParam String uid,
+            @ApiParam(value = "회원이름", required = true) @RequestParam String name) {
+        User user = User.builder()
+                .msrl(msrl)
+                .uid(uid)
+                .name(name)
+                .build();
+        return responseService.getSingleResult(userJpaRepo.save(user));
+    }
+
+    @ApiOperation(value = "회원 삭제", notes = "userId로 회원정보를 삭제한다")
+    @DeleteMapping(value = "/user/{msrl}")
+    public CommonResult delete(
+            @ApiParam(value = "회원번호", required = true) @PathVariable long msrl) {
+        userJpaRepo.deleteById(msrl);
+        // 성공 결과 정보만 필요한경우 getSuccessResult()를 이용하여 결과를 출력한다.
+        return responseService.getSuccessResult();
     }
 }
---
+++ src/main/java/com/greeners/rest/api/model/response/SingleResult.java
@@ -0,0 +1,10 @@
+package com.greeners.rest.api.model.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SingleResult<T> extends CommonResult {
+    private T data;
+}
